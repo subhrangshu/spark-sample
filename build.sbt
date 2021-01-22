1c68f3481998de27fc835ad449a73b578ac58f3d
subhrangshu <lalshu.rnj@gmail.com>
2021-01-22 14:18:53 +0530

    Added support for rdd to save to cassandra and validated. Ready for higher tasks.

--- build.sbt
+++ build.sbt
@@ -8,3 +8,6 @@ scalaVersion := "2.11.8"
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.2" % "provided"
 
 libraryDependencies += "com.datastax.spark" % "spark-cassandra-connector_2.11" % "2.4.2" % "provided"
+
+libraryDependencies += "com.twitter" % "jsr166e" % "1.1.0"
+
--- src/main/scala/compute/Compute.scala
+++ src/main/scala/compute/Compute.scala
@@ -1,9 +1,10 @@
 package compute
 
-import com.datastax.spark.connector.toSparkContextFunctions
+import com.datastax.spark.connector.{SomeColumns, toRDDFunctions, toSparkContextFunctions}
 import org.apache.spark.SparkContext
-import org.apache.spark.sql.{SQLContext, SparkSession}
-import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.sql.SparkSession
+
+case class WordCount(entryid: String, entry1: String, entry2: String, entry3: String)
 
 object Compute {
   def sessiondata(spark: SparkContext, sess: SparkSession) = {
@@ -15,7 +16,8 @@ object Compute {
     println("\n\n\n\n\n Table 2: " + rdd2.first)
     println("\n\n\n\n\n Table 3: " + rdd3.first)
 
-    //println("\n\n\n\n" + sess.sql("desc spark_parallalism_test.test_table1") + "\n\n\n\n")
+    val collection = spark.parallelize(Seq(WordCount("1", "insertion test","insertion test","insertion test")))
+    collection.saveToCassandra("spark_parallalism_test", "test_table2", SomeColumns("entryid", "entry1", "entry2", "entry3"))
   }
 
 
